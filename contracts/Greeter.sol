4ed3615700d5ee1428f9866801701ff821df369c
carlomigueldy <carlomigueldy@gmail.com>
2021-10-19 11:57:30 +0800

    Tut contract done

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,22 +1,57 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
 
 contract Greeter {
-    string private greeting;
+    mapping(address => uint256) public ownerToLuckyNumber;
 
-    constructor(string memory _greeting) {
-        console.log("Deploying a Greeter with greeting:", _greeting);
-        greeting = _greeting;
+    constructor() {
+        console.log("Deployed Greeter by '%s'", msg.sender);
     }
 
-    function greet() public view returns (string memory) {
-        return greeting;
+    function sum(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
     }
 
-    function setGreeting(string memory _greeting) public {
-        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
-        greeting = _greeting;
+    function getMyLuckyNumber() external view returns (uint256) {
+        return ownerToLuckyNumber[msg.sender];
+    }
+
+    modifier luckyNumberGuard() {
+        /// @dev if it's not 0 then owner already has a lucky number
+        require(
+            ownerToLuckyNumber[msg.sender] == 0,
+            "You already have a lucky number."
+        );
+        _;
+    }
+
+    modifier luckyNumberNotZero(uint256 _luckyNumber) {
+        require(_luckyNumber != 0, "Lucky number should not be 0.");
+        _;
+    }
+
+    function saveLuckyNumber(uint256 _luckyNumber)
+        external
+        luckyNumberGuard
+        luckyNumberNotZero(_luckyNumber)
+    {
+        ownerToLuckyNumber[msg.sender] = _luckyNumber;
+    }
+
+    modifier matchPreviousLuckyNumber(uint256 _luckyNumber) {
+        require(
+            ownerToLuckyNumber[msg.sender] == _luckyNumber,
+            "Not your previous lucky number."
+        );
+        _;
+    }
+
+    function setLuckyNumber(uint256 _luckyNumber, uint256 _newLuckyNumber)
+        external
+        matchPreviousLuckyNumber(_luckyNumber)
+    {
+        ownerToLuckyNumber[msg.sender] = _newLuckyNumber;
     }
 }
--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,83 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
+import { Greeter } from "../typechain";
 
 describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
+  let contract: Greeter;
+
+  beforeEach(async () => {
     const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+    contract = await Greeter.deploy();
+  });
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  describe("sum", () => {
+    it("should return 5 when given parameters are 2 and 3", async function () {
+      await contract.deployed();
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+      const sum = await contract.sum(2, 3);
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+      expect(sum).to.be.not.undefined;
+      expect(sum).to.be.not.null;
+      expect(sum).to.be.not.NaN;
+      expect(sum).to.equal(5);
+    });
+  });
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  describe("getMyLuckyNumber", () => {
+    it("should return 5 when given 5", async () => {
+      await contract.deployed();
+
+      await contract.saveLuckyNumber(5);
+      const myLuckyNumber = await contract.getMyLuckyNumber();
+
+      expect(myLuckyNumber).to.be.not.undefined;
+      expect(myLuckyNumber).to.be.not.null;
+      expect(myLuckyNumber.toNumber()).to.equal(5);
+    });
+  });
+
+  describe("saveLuckyNumber", () => {
+    it("should revert with message 'Lucky number should not be 0.', when given 0", async () => {
+      await contract.deployed();
+
+      await expect(contract.saveLuckyNumber(0)).to.be.revertedWith(
+        "Lucky number should not be 0."
+      );
+    });
+
+    it("should revert with message 'You already have a lucky number.', when owner already have saved a lucky number", async () => {
+      await contract.deployed();
+
+      await contract.saveLuckyNumber(6);
+
+      await expect(contract.saveLuckyNumber(7)).to.be.revertedWith(
+        "You already have a lucky number."
+      );
+    });
+  });
+
+  describe("setLuckyNumber", () => {
+    it("should revert with message '', when the given lucky number does not match with their existing lucky number", async () => {
+      await contract.deployed();
+      await contract.saveLuckyNumber(6);
+
+      await expect(contract.setLuckyNumber(8, 99)).to.be.revertedWith(
+        "Not your previous lucky number."
+      );
+    });
+
+    it("should update their lucky number, when given the exact existing lucky number stored", async () => {
+      await contract.deployed();
+      await contract.saveLuckyNumber(2);
+
+      const newLuckyNumber = await contract.getMyLuckyNumber();
+
+      await expect(contract.setLuckyNumber(2, 22)).to.be.not.revertedWith(
+        "Not your previous lucky number."
+      );
+      expect(newLuckyNumber).to.be.not.undefined;
+      expect(newLuckyNumber).to.be.not.null;
+      expect(newLuckyNumber.toNumber()).to.be.equal(2);
+    });
   });
 });
